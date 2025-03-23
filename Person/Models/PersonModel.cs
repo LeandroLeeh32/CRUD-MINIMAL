3a757c229edf1771a070fc8a90860556cea328aa
Leandro.oliveira <leandro.oliveira@valid.com>
2025-03-23 19:21:37 -0300

    CRUD  POST, GET - PUT, DELETE

---
+++ Person/Models/PersonModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Net.NetworkInformation;
+
+namespace Person.Models
+{
+    public class PersonModel
+    {
+
+        public PersonModel(string nome, bool status)
+        {
+            Nome = nome;
+            Status = status;
+            Id = Guid.NewGuid();
+        }
+
+        public void ChangeStatus()
+        {
+            Status = false;
+        }
+
+        public void ChangeName(string name)
+        {
+            Nome = name;
+        }
+
+        public Guid Id { get; set; }
+        public string Nome { get; private set; }
+        public bool Status { get; private set; }
+    }
+
+
+
+
+}
---
+++ Person/Routes/PersonRoute.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.WebEncoders.Testing;
+using Person.Data;
+using Person.Models;
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Person.Routes
+{
+    public static class PersonRoute
+    {
+        public static void PersonRoutes(this WebApplication app)
+        {
+
+            var route = app.MapGroup("person");
+
+            // app.MapGet("", () => new PersonModel("leandro",true));
+
+            route.MapPost("",
+                async (PersonContext context, PersonRequest request) =>
+                {
+                    var person = new PersonModel(request.name, request.status);
+                    await context.AddAsync(person);
+                    await context.SaveChangesAsync();
+                });
+
+
+            route.MapGet("",
+                async (PersonContext context) =>
+                {
+                    var people = await context.People.ToListAsync();
+
+                    if (people == null)
+                        return Results.NotFound("Não encontrado");
+
+                    return Results.Ok(people);
+                });
+
+            route.MapPut("{Id:guid}",
+                async (Guid Id, PersonRequest request, PersonContext context) =>
+                {
+                    var people = await context.People.FindAsync(Id);
+
+                    if (people == null)
+                        return Results.NotFound("Não encontrado");
+
+                    people.ChangeName(request.name);
+                    await context.SaveChangesAsync();
+
+                    return Results.Ok(people);
+
+                });
+
+
+            route.MapDelete("{Id:guid}",
+                async (Guid Id, PersonContext context ) =>
+                {
+                    var people = await context.People.FindAsync(Id);
+                    if (people == null)
+                        return Results.NotFound("Não encontrado");
+
+                    people.ChangeStatus();
+                    await context.SaveChangesAsync();
+
+
+                    return Results.Ok();
+
+                });
+
+
+
+
+
+
+        }
+    }
+}
